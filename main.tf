de1140dce2adfde37f6634d6cc2784d138048862
ricardosoareslacerda <66573448+ricardosoareslacerda@users.noreply.github.com>
2023-12-31 03:57:49 -0300

    Atualizando Repository

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location = var.region
-  repository_id = "labdevops"
+  repository_id = "devopslabclc10-infra"
   description = "Imagens Docker"
   format = "DOCKER"
 }
